23e33d27ff291e9347e81e579fe1463e4370c592
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-02-01 19:45:24 +0100

    Experiment: adapted ß-Spline curve generator to use LibGDX utility classes

--- LeapHover-desktop/src/com/filrouge/leaphover/Main.java
+++ LeapHover-desktop/src/com/filrouge/leaphover/Main.java
@@ -7,10 +7,10 @@ import com.filrouge.leaphover.LeapHover;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "HoverLeap";
+		cfg.title = "LeapHover";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 800;
+		cfg.height = 600;
 		
 		new LwjglApplication(new LeapHover(), cfg);
 	}
--- LeapHover/src/com/filrouge/leaphover/experiments/BSCurve.java
+++ LeapHover/src/com/filrouge/leaphover/experiments/BSCurve.java
@@ -2,32 +2,14 @@ package com.filrouge.leaphover.experiments;
 
 import java.util.ArrayList;
 
-class Point2D {
-	public double x, y;
-	Point2D(double x, double y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	public double distanceTo(Point2D other) {
-		return Math.sqrt( Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2) );
-	}
-	public Point2D scaled(double factor) {
-		return new Point2D(this.x * factor, this.y * factor);
-	}
-	public Point2D moved(Point2D other) {
-		return new Point2D(this.x + other.x, this.y + other.y);
-	}
+import com.badlogic.gdx.math.Vector2;
 
-	public String toString() {
-		return "" + this.x + ", " + this.y + "";
-	}
-}
+class PointList extends ArrayList<Vector2> {
+	private static final long serialVersionUID = -4654318152446499125L;
 
-class PointList extends ArrayList<Point2D> {
 	public String toString() {
 		String result = "";
-		for (Point2D p : this) {
+		for (Vector2 p : this) {
 			result += p + "\n";
 		}
 		return result;
@@ -40,8 +22,10 @@ class PointList extends ArrayList<Point2D> {
  * from its control points.
  * @author Merlin Nimier-David
  */
-class BSCurve {
-
+public class BSCurve {
+	/*
+	 * PROPERTIES
+	 */
 	/** The curve's degree (k=4 => degree=3, cubic curve) */
 	protected final int degree = 4;
 	/** 
@@ -50,7 +34,7 @@ class BSCurve {
 	 * Useful for sampling with constant density. */
 	protected double length;
 	/** Ordered list of the curve's control points */
-	protected ArrayList<Point2D> controlPoints;
+	protected ArrayList<Vector2> controlPoints;
 	/** Nodal vector (by default, t_i = i)) */
 	//protected ArrayList<Point2D> nodalVector;
 	
@@ -64,21 +48,24 @@ class BSCurve {
 	};
 	protected ArrayList<PointList> cachedV;
 	
-	public BSCurve(ArrayList<Point2D> controlPoints) {
+	/* 
+	 * METHODS
+	 */
+	// Constructor
+	public BSCurve(ArrayList<Vector2> controlPoints) {
 		cachedV = new ArrayList<PointList>();
 		// TODO : need at least k control points
 		this.controlPoints = controlPoints;
 		updateLength();
 		cacheVectors();
 	}
-	// TODO : be able to customize degree and nodal vector.
 	
 	protected void updateLength() {
 		length = 0d;
 		if (controlPoints.size() > 1) {
-			Point2D previous = controlPoints.get(0);
+			Vector2 previous = controlPoints.get(0);
 			for(int i = 1; i < controlPoints.size(); ++i) {
-				length += previous.distanceTo(controlPoints.get(i));
+				length += previous.dst(controlPoints.get(i));
 				previous = controlPoints.get(i);
 			}
 		}
@@ -90,18 +77,18 @@ class BSCurve {
 		// V_I (vector of points) = MBS . G_I  (matrix multiplication)
 		cachedV = new ArrayList<PointList>();
 		
-		Point2D result, p;
+		Vector2 result, p;
 		int factor;
 		// For each V_I (one vector of 4 points)
 		for (int I = 3; I < controlPoints.size(); ++I) {
 			cachedV.add(new PointList());
 			// For each point of the result vector V_I
 			for (int i = 0; i < this.degree; ++i) {
-				result = new Point2D(0, 0);
+				result = new Vector2(0, 0);
 				for (int j = 0; j < sixMBS[0].length; ++j) {
 					p = controlPoints.get( I - (3-j) );
 					factor = sixMBS[i][j];
-					result = result.moved(p.scaled(factor));
+					result.add(p.cpy().scl(factor));
 				}
 				cachedV.get(I - 3).add(result); 
 			}
@@ -115,7 +102,7 @@ class BSCurve {
 	 * @param t Parameter between 0 (beginning of the curve) and 1 (end)
 	 * @return Point2D The corresponding point
 	 */
-	public Point2D samplePoint(double t) {
+	public Vector2 samplePoint(double t) {
 		t = (t < 0 ? 0 : t);
 		t = (t > 1 ? 1 : t);
 
@@ -133,27 +120,28 @@ class BSCurve {
 		//      = T * V_I
 		// We cached 6*V_I for performance, so we only need to compute:
 		// Q(t) = 1/6 * T * V_I
-		Point2D Q = new Point2D(0, 0);
+		Vector2 Q = new Vector2(0, 0);
 		double difference = (scaledT - t_I);
-		double factor;
+		float factor;
 		PointList referencePoints = cachedV.get(I - 3);
 		for (int k = 0; k < this.degree; ++k) {
-			factor = Math.pow(difference, this.degree - k - 1);
-			Q = Q.moved( referencePoints.get(k).scaled(factor) );
+			factor = (float)Math.pow(difference, this.degree - k - 1);
+			Q.add( referencePoints.get(k).cpy().scl(factor) );
 		}
 
-		return Q.scaled(1 / (double)6);
+		return Q.scl(1 / 6f);
 	}
 
 	/**
 	 * Use this method to retrieve the desired number of points
 	 * sampled from this beta-spline curve. The curve is sample
-	 * uniformely along its whole length.
+	 * uniformely between control points.
+	 * TODO: be able to sample with constant density along length, and not only control points
 	 * @param n The number of points to retrieve
-	 * @return ArrayList<Point2D>
+	 * @return Vector2[]
 	 */
-	public ArrayList<Point2D> getSamples(int n) {
-		ArrayList<Point2D> result = new ArrayList<Point2D>();
+	public Vector2[] getSamples(int n) {
+		Vector2[] result = new Vector2[n];
 		// We sample the curve uniformely along its whole length
 
 		// For each point that we want to output
@@ -162,46 +150,14 @@ class BSCurve {
 		double t = 0;
 		for (int i = 0; i < n; ++i) {
 			// Sample a point at t
-			result.add(samplePoint(t));
+			result[i] = samplePoint(t);
 			t += delta;
 		}
 		
 		return result;
 	}
-
-	public static void main(String[] args) {
-		
-		// Test control points (at least this.degree)
-		ArrayList<Point2D> testControlPoints = new ArrayList<Point2D>();
-		testControlPoints.add(new Point2D(0, 2));
-		testControlPoints.add(new Point2D(0, 2));
-		testControlPoints.add(new Point2D(0, 2));
-		testControlPoints.add(new Point2D(1, 3));
-		testControlPoints.add(new Point2D(1.5, 6));
-		testControlPoints.add(new Point2D(1.8, 8));
-		testControlPoints.add(new Point2D(2.2, 7.8));
-		testControlPoints.add(new Point2D(2.5, 4.5));
-		testControlPoints.add(new Point2D(4, 3));
-		testControlPoints.add(new Point2D(4.5, 3.1));
-		testControlPoints.add(new Point2D(5.8, 6));
-		testControlPoints.add(new Point2D(6, 9));
-		testControlPoints.add(new Point2D(6, 9));
-		testControlPoints.add(new Point2D(6, 9));
-		
-		BSCurve testCurve = new BSCurve(testControlPoints);
-
-		// Sample points and check they from the above curve
-		System.out.println("=== Sampling extreme and middle points:");
-		System.out.println(testCurve.samplePoint(0));
-		System.out.println(testCurve.samplePoint(0.5));
-		System.out.println(testCurve.samplePoint(1));
-		
-
-		int n = 100;
-		System.out.println("=== Sampling " + n + " points: ");
-		ArrayList<Point2D> testPoints = testCurve.getSamples(n);
-		for (Point2D p : testPoints) {
-			System.out.println(p);
-		}
-	}
+	
+	/*
+	 * GETTERS & SETTERS
+	 */
 }
