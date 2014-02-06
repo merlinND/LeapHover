d70544cceacb7571c0089cc51fb11634669b89de
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-02-06 23:58:46 +0100

    Experiment : added FollowCamera

--- LeapHover/src/com/filrouge/leaphover/LeapHover.java
+++ LeapHover/src/com/filrouge/leaphover/LeapHover.java
@@ -3,25 +3,34 @@ package com.filrouge.leaphover;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.filrouge.leaphover.experiments.FollowCamera;
 import com.filrouge.leaphover.experiments.HillGenerator;
 
 public class LeapHover implements ApplicationListener {
-	private World world;
-	private OrthographicCamera camera;
-	private Box2DDebugRenderer debugRenderer;
+	protected World world;
+	
+	protected float worldWidth = 20f;
+	public final float BLOCK_WIDTH = 2f;
+	
+	protected FollowCamera camera;
+	protected Vector3 initialCameraPosition, maximumCameraPosition;
+	protected Box2DDebugRenderer debugRenderer;
 	
 	/** Rates at which the physical simulation advances */
 	public static final Vector2 GRAVITY = new Vector2(0, -1f);
 	public static final float BOX_STEP = (1 / 60f);  
 	public static final int BOX_VELOCITY_ITERATIONS = 6;  
 	public static final int BOX_POSITION_ITERATIONS = 2; 
+	
+	/** A reference to the main character (for testing purpose) */
+	protected Body hero;
 	
 	@Override
 	public void create() {		
@@ -30,8 +39,10 @@ public class LeapHover implements ApplicationListener {
 		
 		world = new World(GRAVITY, true);
 				
-		camera = new OrthographicCamera(1, h/w);
-		camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0f);
+		camera = new FollowCamera(1, h/w);
+		initialCameraPosition = new Vector3(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0f);
+		maximumCameraPosition = new Vector3(worldWidth - (camera.viewportWidth / 2f), camera.viewportHeight / 2f, 0f);
+		camera.position.set(initialCameraPosition);
 		camera.update();
 		
 		debugRenderer = new Box2DDebugRenderer();
@@ -43,19 +54,24 @@ public class LeapHover implements ApplicationListener {
 		// Ground
 		BodyDef bodyDefinition = new BodyDef();
 		bodyDefinition.type = BodyDef.BodyType.StaticBody;
-		bodyDefinition.position.set(camera.viewportWidth / 2f, 0);
-		Body groundBody = world.createBody(bodyDefinition);
-		HillGenerator.makeHill(groundBody, camera.viewportWidth, camera.viewportHeight);
+		// Generate independant hills, each one with width BLOCK_WIDTH
+		// so as to fill this.worldWidth
+		int n = (int) (worldWidth / BLOCK_WIDTH);
+		for (int i = 0; i < n; i++) {
+			bodyDefinition.position.set(i * BLOCK_WIDTH, 0);
+			Body groundBody = world.createBody(bodyDefinition);
+			HillGenerator.makeHill(groundBody, BLOCK_WIDTH, camera.viewportHeight);			
+		}
 		
 		// Falling box
 		bodyDefinition.type = BodyDef.BodyType.DynamicBody;
-		bodyDefinition.position.set(camera.viewportWidth / 2f, camera.viewportHeight);
-		Body boxBody = world.createBody(bodyDefinition);
+		bodyDefinition.position.set(camera.viewportWidth / 4f, camera.viewportHeight);
+		hero = world.createBody(bodyDefinition);
 		
 		float side = camera.viewportHeight / 20f;
 		PolygonShape polygonShape = new PolygonShape();
 		polygonShape.setAsBox(side, side);
-		boxBody.createFixture(polygonShape, 1);
+		hero.createFixture(polygonShape, 1);
 		
 		polygonShape.dispose();
 	}
@@ -64,11 +80,29 @@ public class LeapHover implements ApplicationListener {
 	public void dispose() {
 		debugRenderer.dispose();
 	}
-
+	
 	@Override
 	public void render() {		
 		Gdx.gl.glClearColor(0, 0.1f, 0.1f, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		Vector2 applicationPoint = hero.getPosition();
+		
+		//hero.applyForce(GRAVITY.cpy().scl(-1f), applicationPoint, false);
+		
+		// If hero gets off screen, reset its position
+		if (hero.getPosition().y < -0.5f) {
+			hero.setTransform(0.1f, camera.viewportHeight, 0f);
+			hero.setLinearVelocity(new Vector2(0f, 0f));
+			hero.setAngularVelocity(0);
+		}
+		
+		// Limit speed
+		//if (hero.getPosition().y <= camera.viewportHeight && hero.getLinearVelocity().x < 0.8f)
+		hero.applyForce(new Vector2(0.003f, 0), applicationPoint, false);
+			
+		// Follow the hero
+		camera.follow(hero.getPosition(), initialCameraPosition, maximumCameraPosition);
 		
 		debugRenderer.render(world, camera.combined);
 		world.step(BOX_STEP, BOX_VELOCITY_ITERATIONS, BOX_POSITION_ITERATIONS);
---
+++ LeapHover/src/com/filrouge/leaphover/experiments/FollowCamera.java
@@ -0,0 +1,50 @@
+package com.filrouge.leaphover.experiments;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+public class FollowCamera extends OrthographicCamera {
+	/*
+	 * PROPERTIES
+	 */
+
+	/* 
+	 * METHODS
+	 */
+	// Constructors
+	public FollowCamera() {
+		super();
+	}
+	public FollowCamera(float viewportWidth, float viewportHeight) {
+		super(viewportWidth, viewportHeight);
+	}
+	
+	/**
+	 * Center the camera on the given position
+	 * TODO : make sure not to cross the world's boundaries
+	 * @param target
+	 */
+	public void follow(Vector2 position, Vector3 min, Vector3 max) {
+		position.x += viewportWidth / 4f;
+		position.y += viewportHeight / 2;
+		
+		if (position.x < min.x)
+			position.x = min.x;
+		else if (position.x > max.x)
+			position.x = max.x;
+		if (position.y < min.y)
+			position.y = min.y;
+		else if (position.y > max.y)
+			position.y = max.y;
+		
+		this.position.x = position.x;
+		this.position.y = position.y;
+		update();
+	}
+	
+	
+	/*
+	 * GETTERS & SETTERS
+	 */
+}
